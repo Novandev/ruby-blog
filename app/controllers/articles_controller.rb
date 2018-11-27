9dc84da501187b2e6f572cef4e7116d221ded8cd
novandev <donovan.adams@students.makeschool.com>
2018-11-27 10:46:56 -0800

    done with drud now doing comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,9 +5,13 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all  # Return all fo the articles
   end
+
+
   #  The crudable get route for posting
   def new # this just registers the new route as a get route
+    @article = Article.new  # This allows the resource to uses the error method from the model
   end
+
 
 
   # The post route for handling submission from the route above
@@ -28,20 +32,37 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])  # this goes into active record and finds a particular post by its ID
   end
 
+
+
   #  Update view route for the article
   def edit
     @article = Article.find(params[:id])
   end
+
+
+
   # update Post route for the update route
   def update
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:id])  # Find the article by it's id
 
-    if @article.update(article_params)
-      redirect_to @article
+    if @article.update(article_params)    # Update the article with the article hash
+      redirect_to @article                # If its successful redirect to this new edited article
     else
-      render 'edit'
+      render 'edit'                       # This renders the edit route if there are any errors
     end
   end
+
+  # It is not necessary to pass all the attributes to update. For example, if @article.update(title: 'A new title') was called, Rails would only update the title attribute, leaving all other attributes untouched.
+
+
+  #  Delete section for the articles section
+  def destroy
+    @article = Article.find(params[:id])  # Finds the article by its ID in the model
+    @article.destroy  # Destroys said article
+
+    redirect_to articles_path # redirect to ALL articles path/ index
+  end
+
 
 
 
