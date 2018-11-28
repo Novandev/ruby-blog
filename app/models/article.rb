f330fc4dd25a0986c86c1c00b366992f8bec01ac
novandev <donovan.adams@students.makeschool.com>
2018-11-27 23:30:45 -0800

    done with all crud

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  has_many :comments  # this creates a one to many association with comments
+  has_many :comments, dependent: :destroy  # this creates a one to many association with comments, and when it deletes it deletes the comments as well
   validates :title, presence: true,   # This validation makes sure that it exusts and usnt just an empty string
             length: { minimum: 5 }    # This makes sure the length is at least 5 char
 end
